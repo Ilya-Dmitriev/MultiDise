51892094adeda8749fee2e58186c2b39b94e8ad3
Ilya Dmitriev <dmitrievilyawork@gmail.com>
2022-08-29 22:54:39 +0300

    Spell filters complete

--- src/components/UI/BaseInput/MainInput.jsx
+++ src/components/UI/BaseInput/MainInput.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import clsx from 'clsx';
 import classes from './MainInput.module.scss';
 
 export const MainInput = ({ placeholder = 'Input', className, ...props }) => {
+  const inputClasses = clsx(classes.base_input, className);
   return (
-    <input className={`${classes.base_input} ${className}`} placeholder={placeholder} {...props} />
+    <input className={inputClasses} placeholder={placeholder} {...props} />
   );
 };
